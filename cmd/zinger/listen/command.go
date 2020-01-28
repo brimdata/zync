d4d137ab1bb2e18e3323d4c664998331fd887c67
Henri DF <henridf@gmail.com>
2020-01-28 12:45:23 +0100

    move kafka related args under their respective commands

--- cmd/zinger/listen/command.go
+++ cmd/zinger/listen/command.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mccanne/charm"
 	"github.com/mccanne/zinger/cmd/zinger/root"
@@ -29,19 +30,33 @@ func init() {
 
 type Command struct {
 	*root.Command
-	listenAddr string
+	kafkaCluster    string
+	registryCluster string
+	Topic           string
+	Namespace       string
+	Subject         string
+	listenAddr      string
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 	c := &Command{Command: parent.(*root.Command)}
+	f.StringVar(&c.kafkaCluster, "k", "localhost:9092", "[addr]:port list of one or more kafka servers")
+	f.StringVar(&c.registryCluster, "r", "localhost:8081", "[addr]:port list of one more kafka registry servers")
+	//XXX change these defaults?
+	f.StringVar(&c.Topic, "t", "kavro-test", "subject name for kafka schema registry")
+	f.StringVar(&c.Namespace, "n", "com.example", "namespace to use when creating new schemas")
+	f.StringVar(&c.Subject, "s", "kavrotest-value", "subject name for kafka schema registry")
 	f.StringVar(&c.listenAddr, "l", ":9890", "[addr]:port to listen on")
 	return c, nil
 }
 
+func servers(s string) []string {
+	return strings.Split(s, ",")
+}
+
 func (c *Command) Run(args []string) error {
-	servers := c.KafkaCluster()
-	reg := registry.NewConnection(c.Subject, c.RegistryCluster())
-	producer, err := zinger.NewProducer(servers, reg, c.Topic, c.Namespace)
+	reg := registry.NewConnection(c.Subject, servers(c.registryCluster))
+	producer, err := zinger.NewProducer(servers(c.kafkaCluster), reg, c.Topic, c.Namespace)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
