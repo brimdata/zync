6ed2b8084b1aca2da9d61692bdf35e8ec0ce0348
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-12-08 11:46:15 -0500

    encode repeated record as reference in zavro.EncodeSchema
    For a Zed type containing two identical records (e.g, {a:{},b:{}})
    zavro.EncodeSchema returns a schema containing two records with the same
    name, which is something schema registries don't allow.  Change
    EncodeSchema to use avro.RecursiveSchema for any Zed record type
    previously encountered so that subsequent appearances of a record type
    become references (rather than duplicates) in the returned schema.

---
+++ zavro/schema_test.go
@@ -0,0 +1,49 @@
+package zavro
+
+import (
+	"testing"
+
+	"github.com/brimdata/zed"
+	"github.com/brimdata/zed/zson"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncodeSchemaRepeatedRecordBecomesReference(t *testing.T) {
+	typ, err := zson.ParseType(zed.NewContext(), "{a:{},b:{}}")
+	require.NoError(t, err)
+	schema, err := EncodeSchema(typ, "namespace")
+	require.NoError(t, err)
+	const expected = `
+{
+    "type": "record",
+    "namespace": "namespace",
+    "name": "zng_2d7e63a29282715120ae93531a98c9ef",
+    "doc": "Created by zync from zng type {a:{},b:{}}",
+    "fields": [
+        {
+            "name": "a",
+            "default": null,
+            "type": [
+                "null",
+                {
+                    "type": "record",
+                    "namespace": "namespace",
+                    "name": "zng_99914b932bd37a50b983c5e7c90ae93b",
+                    "doc": "Created by zync from zng type {}",
+                    "fields": null
+                }
+            ]
+        },
+        {
+            "name": "b",
+            "default": null,
+            "type": [
+                "null",
+                "zng_99914b932bd37a50b983c5e7c90ae93b"
+            ]
+        }
+    ]
+}`
+	assert.JSONEq(t, expected, schema.String())
+}
